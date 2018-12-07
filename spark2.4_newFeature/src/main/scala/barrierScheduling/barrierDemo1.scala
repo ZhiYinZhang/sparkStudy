c54bb2345b72317ea2a217862dbf7d88361df91a
2454099127@qq.com <2454099127@qq.com>
2018-12-07 18:00:47 +0800

    friday

--- spark2.4_newFeature/src/main/scala/barrierScheduling/barrierDemo1.scala
+++ spark2.4_newFeature/src/main/scala/barrierScheduling/barrierDemo1.scala
@@ -1,6 +1,8 @@
 package barrierScheduling
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.BarrierTaskContext
+import org.apache.spark.rdd.{RDD, RDDBarrier}
+import org.apache.spark.sql.{Row, SparkSession}
 
 object barrierDemo1 {
     def main(args:Array[String]):Unit={
@@ -11,7 +13,30 @@ object barrierDemo1 {
       spark.sparkContext.setLogLevel("WARN")
       import spark.implicits._
 
-      
+      val df = spark.read
+        .format("csv")
+        .option("header",true)
+        .option("inferSchema",true)
+        .load("E:\\pythonProject\\dataset\\model01_train.csv")
+      df.show(10)
+      val a = df.rdd
+      val a1: RDD[Row] = a.repartition(3)
+      println(a1.partitions.length)
+      val ba: RDDBarrier[Row] = a1.barrier()
+      val result: RDD[Row] = ba.mapPartitions { iter =>
+        println("start barrier")
+        val context = BarrierTaskContext.get()
+        if (context.partitionId() == 0) {
+             println(context.attemptNumber())
+             println(context.getTaskInfos())
+             println(context.stageId())
+             println(context.taskAttemptId())
+             println(context.taskMetrics())
+        }
+        context.barrier()
+        iter
+      }
+      result.collect()
 
 
 
