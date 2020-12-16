9d6c7c5d116fd65f2bc74ddcc0945e5658cfb7d3
ZhiYin Zhang <2454099127@qq.com>
2020-12-16 10:22:54 +0800

    修改工程pom文件，scala改成2.11.12版本

---
+++ DF_Operate/src/main/scala/idMapping/logDataIdmp.scala
@@ -0,0 +1,69 @@
+package idMapping
+
+import org.apache.commons.lang.StringUtils
+import org.apache.spark.graphx.{Edge, Graph}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{array, col}
+/**
+ * 三类埋点日志的id映射计算程序
+ * 没考虑滚动整合
+ */
+object logDataIdmp {
+  def main(args: Array[String]): Unit = {
+    val spark=SparkSession.builder()
+      .appName(this.getClass.getSimpleName)
+      .master("local[2]")
+      .getOrCreate()
+    val sc=spark.sparkContext
+    sc.setLogLevel("warn")
+    import spark.implicits._
+
+    val path="e://data//logData//"
+    val appData=spark.read.option("header",true).csv(path+"app.csv")
+    val webData=spark.read.option("header",true).csv(path+"web.csv")
+    val wechatData=spark.read.option("header",true).csv(path+"wechat.csv")
+
+    val cols=appData.columns.map(col(_))
+    //将所有标识列放到数组中,并拼接所有日志数据
+    val allTag=appData.select(array(cols:_*).alias("allTag"))
+      .union(webData.select(array(cols:_*).alias("allTag")))
+      .union(wechatData.select(array(cols:_*).alias("allTag")))
+
+    //1.构造点rdd
+    val vertices: RDD[(Long, String)] = allTag.rdd.flatMap(row => {
+      //过滤掉空的数据
+      val filter = row.getSeq[String](0).filter(StringUtils.isNotBlank(_))
+      filter.map(tag => (tag.hashCode.toLong, tag))
+    })
+
+    //2.构造边rdd
+    /**
+     *过滤弱关联边：即过滤掉出现次数比较少的边，如其他的人使用我的手机登陆了账号，那么应该过滤掉他,这个阈值就是经验了
+     *在构造边的时候应该两两连接，然后再去统计每个边的数量，过滤掉数量少的
+     */
+    val edges=allTag.rdd.flatMap(row=>{
+      //过滤掉空的数据
+      val filter=row.getSeq[String](0).filter(StringUtils.isNotBlank(_))
+      //使用双层for循环
+      for(i<-0 until filter.size-1;j<-i+1 until filter.size) yield Edge(filter(i).hashCode.toLong,filter(j).hashCode.toLong,"")
+    })
+    val filter_edges=edges.map(edge=>(edge,1))
+      .reduceByKey(_+_)//计数
+      .filter(_._2>2)//过滤掉次数小于等于2的边
+      .map(tp=>tp._1)
+
+    //3.构造图
+    val graph=Graph(vertices,filter_edges)
+    val graph2=graph.connectedComponents()
+
+
+    val vertices1=graph2.vertices
+
+    //保存结果
+    vertices1.toDF("tag_hashcode","guid")
+      .write
+      .parquet("e:://data//idMapping_res//20201215")
+
+  }
+}
